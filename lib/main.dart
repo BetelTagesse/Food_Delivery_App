74037a2f6c365df239b367390a201d862c8ebe6b
BetelTagesse <BetelTagesse367@gmail.com>
2024-05-30 13:00:00 +0300

    fixed the bug

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("food"),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
     );
