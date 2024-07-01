038da0124c3cbdc3875343f5adc2ef8e90eecaa6
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-01 13:00:00 +0300

    fixed the bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/pages/login_page.dart';
+import 'package:food_delivery_app/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,43 +18,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("food"),
-      ),
-      body: Center(
-        child: Row(
-          children: [
-            Text("food"),
-            Text("food"),
-            Text("food"),
-            Text("food"),
-            Text("food"),
-          ],
-        ),
-      ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      home: const LoginPage(),
     );
   }
 }
