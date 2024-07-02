691696536f60706548d6ebaa29984df59bf60ae1
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-02 13:00:00 +0300

    login_page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.lock),
+          Text(
+            'Food Delivery App',
+            style:
+                TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+          ),
+          MyTextField(
+            controller: emailController,
+            obscureText: false,
+            hintText: 'Email',
+          ),
+          MyTextField(
+            controller: passwordController,
+            obscureText: true,
+            hintText: 'Password',
+          ),
+        ],
+      ),
     );
   }
 }
