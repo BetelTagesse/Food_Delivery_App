0d0db50b860730448925127ff359398454a21d7c
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-05 13:00:00 +0300

    drawer

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -10,6 +10,8 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Setting')),
+    );
   }
 }
