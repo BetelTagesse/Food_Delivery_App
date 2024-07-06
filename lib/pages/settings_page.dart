21df176953aeaf51b7d7514da2626ba6306b18da
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-06 13:00:00 +0300

    drawer

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -17,6 +17,8 @@ class _SettingsPageState extends State<SettingsPage> {
         body: Column(
           children: [
             Container(
+              padding: const EdgeInsets.fromLTRB(18.0, 25.0, 30.0, 25),
+              margin: const EdgeInsets.all(25),
               decoration:
                   BoxDecoration(color: Theme.of(context).colorScheme.secondary),
               child: Row(
