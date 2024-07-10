11c9133b5fe21246630a0178b88eb4eceb0b290e
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-10 13:00:00 +0300

    drawer

--- lib/components/my_current_location.dart
+++ lib/components/my_current_location.dart
@@ -6,6 +6,7 @@ class MyCurrentLocation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text('Deliver now'),
         Row(
