ad78ccce2d7ce18b925464f48ed8f35ec6cd4f0d
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-11 13:00:00 +0300

    drawer

--- lib/components/my_current_location.dart
+++ lib/components/my_current_location.dart
@@ -5,17 +5,44 @@ class MyCurrentLocation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Deliver now'),
-        Row(
-          children: [
-            Text('6901 HollyWood Blv'),
-            Icon(Icons.keyboard_arrow_down_rounded)
-          ],
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Deliver now',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          GestureDetector(
+            onTap: () => openLocationSearchBox(context),
+            child: Row(
+              children: [
+                Text(
+                  '6901 HollyWood Blv',
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.keyboard_arrow_down_rounded)
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
+}
+
+void openLocationSearchBox(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+        title: Text('Your location'),
+        content: TextField(
+          decoration: const InputDecoration(hintText: 'Search address..'),
+        )),
+  );
 }
