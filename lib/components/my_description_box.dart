bd9ab984e06bee1ba057958a94edc6868f2c1d42
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-12 13:00:00 +0300

    drawer

--- lib/components/my_description_box.dart
+++ lib/components/my_description_box.dart
@@ -6,21 +6,35 @@ class MyDescriptionBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var myPrimaryTextStyle =
+        TextStyle(color: Theme.of(context).colorScheme.inversePrimary);
+    var mySecondaryTextStyle =
+        TextStyle(color: Theme.of(context).colorScheme.primary);
     return Container(
       padding: EdgeInsets.all(25),
+      margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).colorScheme.secondary,
-        ),
-      ),
+          border: Border.all(
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          borderRadius: BorderRadius.circular(8)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Column(
-            children: [Text('\$2.99'), Text('Delivery Fee')],
+            children: [
+              Text('\$2.99', style: myPrimaryTextStyle),
+              Text(
+                'Delivery Fee',
+                style: mySecondaryTextStyle,
+              )
+            ],
           ),
           Column(
-            children: [Text('10-25 min'), Text('Delivery time')],
+            children: [
+              Text('10-25 min', style: mySecondaryTextStyle),
+              Text('Delivery time', style: myPrimaryTextStyle)
+            ],
           )
         ],
       ),
