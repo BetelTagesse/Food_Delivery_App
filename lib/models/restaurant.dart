b960a04ca4716c33869c8a066728f6d30fc28c7a
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-14 15:00:00 +0300

    added models

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,5 +1,14 @@
 import 'package:food_delivery_app/models/food.dart';
 
 class Restaurant {
-  final List<Food> _menu = [];
+  final List<Food> _menu = [
+    Food(
+        description:
+            'A juicy beef patty with melted cheese, lettuce, tomato and hins of onion and pickle .',
+        imagePath: 'lib/images/burgers/cheese_burger.png',
+        price: price,
+        category: category,
+        name: 'classic cheeseburger',
+        availableAddons: availableAddons)
+  ];
 }
