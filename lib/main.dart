af707bd0c63451e8aa37a932f1c787aadca9eae1
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-18 15:00:00 +0300

    tabbar fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/auth/login_or_register.dart';
+import 'package:food_delivery_app/models/restaurant.dart';
 import 'package:food_delivery_app/pages/login_page.dart';
 import 'package:food_delivery_app/pages/register_page.dart';
 import 'package:food_delivery_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    child: const MyApp(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Restaurant(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
