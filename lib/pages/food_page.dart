2ca47af9edda9456b1c06284af7d941ad2496c43
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-21 15:00:00 +0300

    tiles done

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -18,31 +18,45 @@ class _FoodPageState extends State<FoodPage> {
 
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ClipRRect(
-              child: Image.asset(
-                widget.food.imagePath,
-                width: double.infinity,
-                height: screenHeight * 0.5,
-                fit: BoxFit.cover,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              ClipRRect(
+                child: Image.asset(
+                  widget.food.imagePath,
+                  width: double.infinity,
+                  height: screenHeight * 0.5,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Text(widget.food.name),
-            Text(widget.food.description),
-            ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: widget.food.availableAddons.length,
-                itemBuilder: (context, index) {
-                  Addon addon = widget.food.availableAddons[index];
+              Text(widget.food.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  )),
+              SizedBox(height: 10),
+              Text(
+                widget.food.description,
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.primary),
+              ),
+              SizedBox(height: 10),
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: widget.food.availableAddons.length,
+                  itemBuilder: (context, index) {
+                    Addon addon = widget.food.availableAddons[index];
 
-                  return CheckboxListTile(
-                      title: Text(addon.name),
-                      value: false,
-                      onChanged: (value) {});
-                }),
-          ],
+                    return CheckboxListTile(
+                        title: Text(addon.name),
+                        subtitle: Text(addon.price.toString()),
+                        value: false,
+                        onChanged: (value) {});
+                  }),
+            ],
+          ),
         ),
       ),
     );
