b3cd0e62d5ab98915e0eaa67dae5a1d7b53cc36b
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-31 15:00:00 +0300

    food page  done

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -7,4 +7,10 @@ class CartItem {
 
   CartItem(
       {required this.food, required this.selectedAddons, this.quantity = 1});
+
+  double get totalPrice {
+    double addonsPrice =
+        selectedAddons.fold(0, (sum, addon) => sum + addon.price);
+    return (food.price + addonsPrice) * quantity;
+  }
 }
