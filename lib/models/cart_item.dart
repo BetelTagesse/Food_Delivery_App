1046f2f21308b1304ff9b93928cdae8757847627
BetelTagesse <BetelTagesse367@gmail.com>
2024-07-31 15:00:00 +0300

    food page  done

---
+++ lib/models/cart_item.dart
@@ -0,0 +1,10 @@
+import 'package:food_delivery_app/models/food.dart';
+
+class CartItem {
+  Food food;
+  List<Addon> selectedAddons;
+  int quantity;
+
+  CartItem(
+      {required this.food, required this.selectedAddons, this.quantity = 1});
+}
