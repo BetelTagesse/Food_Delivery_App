5acc739907880fa54b30f8552063e145ecbb2276
BetelTagesse <BetelTagesse367@gmail.com>
2024-08-06 15:00:00 +0300

    cart page  done

---
+++ lib/components/mycart_tile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/models/cart_item.dart';
+import 'package:provider/provider.dart';
+
+import '../models/restaurant.dart';
+
+class MycartTile extends StatelessWidget {
+  final CartItem cartItem;
+  const MycartTile({super.key, required this.cartItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) {
+        return Container(
+            child: Column(
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  (cartItem.food.imagePath),
+                ),
+              ],
+            )
+          ],
+        ));
+      },
+    );
+  }
+}
